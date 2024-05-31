3bed040dc94d02bea6fbf5ccb974dea52cc96546
marozauandrei77@gmail.com <marozauandrei77@gmail.com>
2024-05-31 03:30:09 +0300

    Добавьте файлы проекта.

---
+++ LabB_6.2(6)_Marozau/LabB_6.2(6)_Marozau.cpp
@@ -0,0 +1,10 @@
+﻿// LabB_6.2(6)_Marozau.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
